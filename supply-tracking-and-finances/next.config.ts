e3fb607a468e5961a595bd082880bb67e22f409d
DulranS <127298642+DulranS@users.noreply.github.com>
2025-12-11 03:38:24 +0530

    wtf

--- supply-tracking-and-finances/next.config.ts
+++ supply-tracking-and-finances/next.config.ts
@@ -1,5 +1,12 @@
-// next.config.ts
-const nextConfig = {
+import type { NextConfig } from "next";
+
+interface ExtendedConfig extends NextConfig {
+  eslint?: {
+    ignoreDuringBuilds?: boolean;
+  };
+}
+
+const nextConfig: ExtendedConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
