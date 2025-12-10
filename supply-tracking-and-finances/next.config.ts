de62dc015f69ca693c842319e3dfb541cf0d3702
DulranS <127298642+DulranS@users.noreply.github.com>
2025-12-11 03:50:02 +0530

    qwt

---
+++ supply-tracking-and-finances/next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  
+     images: {
+      
+       unoptimized: true, // temporary for testing
+     },
+};
+
+export default nextConfig;
