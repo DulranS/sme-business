540fcf7585c668e4e01b3179839d5ba38f3c8f23
DulranS <127298642+DulranS@users.noreply.github.com>
2026-03-05 01:36:36 +0530

    l

--- scrape-mails/next.config.mjs
+++ scrape-mails/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/format_DISABLED/**', '**/node_modules/**'],
+    };
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
