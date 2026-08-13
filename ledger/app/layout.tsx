5f54d49f57c149e576ad4b3e02aa22f863c29e6b
DulranS <127298642+DulranS@users.noreply.github.com>
2026-08-13 14:16:29 +0530

    fixed i think

--- ledger/app/layout.tsx
+++ ledger/app/layout.tsx
@@ -34,7 +34,7 @@ export const viewport: Viewport = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${display.variable} ${body.variable} ${mono.variable}`}>
+    <html lang="en" className={`${display.variable} ${body.variable} ${mono.variable}`} suppressHydrationWarning>
       <body>
         <AuthProvider>
           <DataProvider>
